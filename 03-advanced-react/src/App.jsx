a4ef3e77ab584a4b613b38a199f940334c826004
X_Trial_Test <1trialandtest@gmail.com>
2023-03-02 21:05:59 +0300

    useState

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,7 +1,8 @@
+import Ex from './tutorial/01-useState/starter/03-useState-array';
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced React</h2>
+      <Ex />
     </div>
   );
 }
--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,42 @@
+import { data } from '../../../data';
+import { useState } from 'react';
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  let [name, setName] = useState(data);
+  let removeItem = (singleId) => {
+    let Newname = name.filter(({ id }) => {
+      if (singleId != id) {
+        return singleId;
+      }
+    });
+    setName(Newname);
+  };
+  return (
+    <>
+      {name.map(({ name, id }) => {
+        console.log(id);
+        return (
+          <>
+            <h3 key={id}>{name}</h3>
+            <button
+              onClick={() => {
+                removeItem(id);
+              }}
+            >
+              remove
+            </button>
+          </>
+        );
+      })}
+      <button
+        style={{ display: 'block' }}
+        onClick={() => {
+          setName([]);
+        }}
+      >
+        remove All
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
